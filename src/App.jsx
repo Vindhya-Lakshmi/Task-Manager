0d19ccff832f39309b3b589ca89c56172d61e11c
Vindhya Lakshmi <vindhyalakshmiofficial@gmail.com>
2025-09-30 20:31:33 +0530

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Navbar from "./components/Navbar";
+
+
+
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline bg-amber-300">Hello world!</h1>
+    <Navbar/>
+      <h1></h1>
     </>
   );
 }
